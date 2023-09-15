91a2a152591f2a2833ebec82f45a3bdf6cbcd6e4
Seungil Kim <ksi990302@gmail.com>
2023-09-15 20:00:17 +0900

    ⚡️ Update: openai sdk v4로 마이그레이션
    #

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,18 +1,19 @@
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
+import { Prompt } from './prompt';
 
-const config = new Configuration({
+const config = {
   apiKey: process.env.OPENAI_API_KEY,
-});
+};
 
-const openai = new OpenAIApi(config);
+const openai = new OpenAI(config);
 
 export const getEmbedding = async (text: string) => {
   try {
-    const result = await openai.createEmbedding({
+    const result = await openai.embeddings.create({
       model: 'text-embedding-ada-002',
       input: text,
     });
-    return result.data.data[0].embedding;
+    return result.data[0].embedding;
   } catch (e) {
     if (e.response) {
       console.error(e.response.status);
@@ -40,69 +41,25 @@ export const getSummary = async (title: string, content: string) => {
     inputContent = content;
   }
 
-  const input = title + '\n' + inputContent;
-
   try {
     const model = 'gpt-3.5-turbo';
-    const language = 'Korean';
-    const prompt = `You are a helpful AI assistant for a busy journalist.
-  The journalist has asked you to write a summary and hashtags of the following article.
-  A one-line summary is a sentence that expresses the contents of the entire document, not the title of the document. It should be a concise one sentence.
-  The summary is a summary of the document and should be concise and concise, within one paragraph.
-  Craft a summary that is detailed, thorough, in-depth, and complex, while maintaining clarity and conciseness.
-  Incorporate main ideas and essential information, eliminating extraneous language and focusing on critical aspects.
-  Rely strictly on the provided text, without including external information.
-  Tags should be representative words of the document rather than peripheral words.
-  Tags should be words that can be used to classify multiple documents.
-  You MUST use presented language in the article for the summary and hashtags.
-  ---
-  Language: ${language}
-  Article: \n
-  `;
-
-    const completion = await openai.createChatCompletion({
+    const language = 'Korean'; // TODO: 사용자가 입력할 수 있도록 변경
+    const prompt: Prompt = new Prompt(language, 0, title, inputContent);
+    const completion = await openai.chat.completions.create({
       model: model,
       messages: [
-        { role: 'system', content: prompt },
-        { role: 'user', content: input },
+        { role: 'system', content: prompt.prompt },
+        { role: 'user', content: prompt.input },
       ],
       temperature: 0,
-      functions: [
-        {
-          name: 'insertMetadata',
-          description: 'Inserts summary and hashtags into the article metadata',
-          parameters: {
-            type: 'object',
-            properties: {
-              oneLineSummary: {
-                type: 'string',
-                description:
-                  'a concise summary of the entire document within one sentence',
-              },
-              summary: {
-                type: 'string',
-                description:
-                  'concise, one-paragraph or less summary of the entire document',
-              },
-              hashtags: {
-                type: 'array',
-                items: {
-                  type: 'string',
-                },
-                description: 'A list of hashtags for the article',
-              },
-            },
-            required: ['shortSummary', 'summary', 'hashtags'],
-          },
-        },
-      ],
+      functions: prompt.functions,
       function_call: {
         name: 'insertMetadata',
       },
     });
 
     const result: { oneLineSummary: string; summary: string; hashtags: [] } =
-      JSON.parse(completion.data.choices[0].message.function_call.arguments);
+      JSON.parse(completion.choices[0].message.function_call.arguments);
 
     return {
       summary: result.oneLineSummary + '\n' + result.summary,
